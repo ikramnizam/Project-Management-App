68b0e610d89787aafac0dee4fb21048a6afc4ea4
ikramnizam <ikrmnzm@gmail.com>
2024-08-13 15:47:15 +0800

    Create Controllers and Define Routes

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -21,8 +21,25 @@ export default function Authenticated({ user, header, children }) {
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink href={route('dashboard')} active={route().current('dashboard')}>
+                                <NavLink 
+                                href={route('dashboard')} 
+                                active={route().current('dashboard')}>
                                     Dashboard
+                                </NavLink>
+                                <NavLink 
+                                href={route('project.index')} 
+                                active={route().current('project.index')}>
+                                    Project
+                                </NavLink>
+                                <NavLink 
+                                href={route('task.index')} 
+                                active={route().current('task.index')}>
+                                   All Task
+                                </NavLink>
+                                <NavLink 
+                                href={route('user.index')} 
+                                active={route().current('user.index')}>
+                                   Users
                                 </NavLink>
                             </div>
                         </div>
